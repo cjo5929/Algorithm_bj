6fba7ebcbae77eed6a4bf2456b3b1bee36d2c171
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-01-28 01:23:06 +0900

    [Silver IV] Title: 주몽, Time: 204 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static StringTokenizer st;
-	static int n, m, cnt;
+	static int n, m, cnt, start, end, sum;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,17 +21,44 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		
+		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 0; i < n - 1; i++) {
-			for(int j = i + 1; j < n; j++) {
-				if (arr[i] + arr[j] == m) {
-					cnt++;
-				}
+		Arrays.sort(arr);
+		start = 0;
+		end = n - 1;
+		sum = arr[start] + arr[end];
+		while(start != end) {
+			if(sum < m) {
+				
+				start++;
+				sum = arr[start] + arr[end];
+				
+			}else if(sum > m) {
+				
+				end--;
+				sum = arr[start] + arr[end];
+				
+			}else {
+				cnt++;
+				start++;
+				
+				sum = arr[start] + arr[end];
 			}
+			
 		}
+		
+		
+//		2중 반복문으로 완탐
+//		for(int i = 0; i < n - 1; i++) {
+//			for(int j = i + 1; j < n; j++) {
+//				if (arr[i] + arr[j] == m) {
+//					cnt++;
+//				}
+//			}
+//		}
 		
 	
 		System.out.println(cnt);
