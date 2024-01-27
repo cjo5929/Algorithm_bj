f828e456a7b635f97bb686551503403f29f4d151
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-01-28 01:30:35 +0900

    [Silver IV] Title: 주몽, Time: 200 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -5,52 +5,51 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static StringTokenizer st;
 	static int n, m, cnt, start, end, sum;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		
-		
+
 		int[] arr = new int[n];
-		
-		
+
 		st = new StringTokenizer(br.readLine());
-		
-		
-		for(int i = 0; i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
+//		투포인터를 위해 정렬
 		Arrays.sort(arr);
 		start = 0;
 		end = n - 1;
-		sum = arr[start] + arr[end];
-		while(start != end) {
-			if(sum < m) {
-				
+
+//		투포인터
+		while (start != end) {
+			sum = arr[start] + arr[end];
+//			sum 이 목표 값 m보다 작다면 start값 올리기
+			if (sum < m) {
+
 				start++;
-				sum = arr[start] + arr[end];
-				
-			}else if(sum > m) {
-				
+
+//			sum 이 목표 값 m보다 크다면 end값 올리기
+			} else if (sum > m) {
+
 				end--;
-				sum = arr[start] + arr[end];
-				
-			}else {
+
+//			같다면 cnt, start 값 올리기
+			} else {
 				cnt++;
 				start++;
-				
-				sum = arr[start] + arr[end];
+
 			}
-			
+
 		}
-		
-		
+
 //		2중 반복문으로 완탐
 //		for(int i = 0; i < n - 1; i++) {
 //			for(int j = i + 1; j < n; j++) {
@@ -59,12 +58,8 @@ public class Main {
 //				}
 //			}
 //		}
-		
-	
+
 		System.out.println(cnt);
-		
-		
-		
 
 	}
 
