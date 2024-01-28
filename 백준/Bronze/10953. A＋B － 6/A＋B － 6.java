8d011568b713f14407768c0687d681fbec31e1e9
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-01-28 14:34:36 +0900

    [Bronze III] Title: A+B - 6, Time: 148 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken(","));
+			int y = Integer.parseInt(st.nextToken(","));
+			
+			sb.append(x+y + "\n");
+			
+		}
+		
+		System.out.println(sb);
+		
+
+	}
+
+}
