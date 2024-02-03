be7d0b247c0c46e21ac128acf06f28894bc9370f
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-03 15:51:24 +0900

    [Silver I] Title: 절댓값 힙, Time: 324 ms, Memory: 25376 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,3 +1,14 @@
+/**
+ * @author 강민서
+ * @date 2024.02.03
+ * @link https://www.acmicpc.net/problem/11286
+ * @keyword_solution  절댓값 힙 -> 우선순위 큐의 정렬 기준을 절댓값으로 교체 
+ * @input N(1≤N≤100,000)
+ * @output 0을 입력 받으면 출력 -> StringBuilder로 한 번에 출력
+ * @time_complex  O(NlogN)
+ * @perf 25412 340
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,13 +26,14 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int N = Integer.parseInt(br.readLine());
 
-		PriorityQueue<Integer> priorityQ = new PriorityQueue(new Comparator<Integer>() {
+		PriorityQueue<Integer> priorityQ = new PriorityQueue<Integer>(new Comparator<Integer>() {
+			
 			@Override
 			public int compare(Integer o1, Integer o2) {
-				if (Math.abs(o1) > Math.abs(o2)) {
-					return Math.abs(o1) - Math.abs(o2);
+				if (Math.abs(o1) > Math.abs(o2)) {  // o1의 절댓값이 더 크다면 
+					return 1;
 				} else if (Math.abs(o1) == Math.abs(o2)) {
-					return o1 - o2;
+					return o1.compareTo(o2);
 				} else {
 					return -1;
 				}
