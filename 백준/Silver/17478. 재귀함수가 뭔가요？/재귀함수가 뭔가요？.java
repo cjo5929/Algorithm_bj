6c2b422e1971117261bb5cb80147e94de6b95c93
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-03 14:02:05 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 152 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -4,13 +4,19 @@ import java.io.InputStreamReader;
 
 /**
  * @author 강민서
- * @date 
- * @link
- * @keyword_solution  
- * @input 
- * @output   
- * @time_complex  
- * @perf 
+ * @date 0129
+ * @link https://www.acmicpc.net/problem/17478
+ * @keyword_solution  ________라고 답변하였지. => 뒤에 점점 _가 줄고 라고 답변하였지만 반복하는거 보니 재귀호출 하는 함수 뒤에 쓰여짐
+ * "____"재귀함수가 뭔가요?"
+____"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
+____마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
+____그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.""
+가 반복되는거 보니 재귀호출 안에 있음 => ____를 추가하면서 돌기
+n == 0이 될 때 멈춤
+ * @input N(1 ≤ N ≤ 50)
+ * @output String
+ * @time_complex  (n)
+ * @perf 14828kb 156ms
  */
 
 public class Main {
