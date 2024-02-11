f216b3587d5bbd866b4bf090977dd14ad1239488
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-11 17:57:14 +0900

    [Gold IV] Title: 타임머신, Time: 304 ms, Memory: 19564 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,16 +1,19 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Edge {
-	int vertex;
+	int start;
 	int value;
+	int end;
 
-	public Edge(int vertex, int value) {
-		this.vertex = vertex;
+	public Edge(int start, int value, int end) {
+		this.start = start;
 		this.value = value;
+		this.end = end;
 	}
 
 }
@@ -21,21 +24,18 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	static int N, M;
-	static ArrayList<Edge>[] list;
+	static Edge[] list;
 	static long[] dist;
 
 	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());		
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken()); // 도시의 개수
 		M = Integer.parseInt(st.nextToken()); // 버스 노선의 개수
 
-		list = new ArrayList[N + 1];
+		list = new Edge[M + 1];
 		dist = new long[N + 1];
 
-		for (int i = 0; i <= N; i++) {
-			list[i] = new ArrayList<Edge>();
-			dist[i] = Integer.MAX_VALUE;
-		}
+		Arrays.fill(dist, Integer.MAX_VALUE);
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -43,65 +43,53 @@ public class Main {
 			int B = Integer.parseInt(st.nextToken());
 			int C = Integer.parseInt(st.nextToken());
 
-			list[A].add(new Edge(B, C));
+			list[i] = new Edge(A, C, B);
 		}
-		
-		if(belManFord()) {
+
+		if (belManFord()) {
 			sb.append(-1 + "\n");
-		}else {
-			for(int i = 2; i <= N; i++) {
-				if(dist[i] == Integer.MAX_VALUE) sb.append(-1 + "\n");
-				else sb.append(dist[i] + "\n");
+		} else {
+			for (int i = 2; i <= N; i++) {
+				if (dist[i] == Integer.MAX_VALUE)
+					sb.append(-1 + "\n");
+				else
+					sb.append(dist[i] + "\n");
 			}
 		}
-		
+
 		System.out.println(sb);
 
 	}
 
 	static boolean belManFord() {
 		dist[1] = 0; // 시작 도시 1
-		boolean update = false;
 
 //		N - 1번 돌기 => 전체 도시 탐색 1회
 		for (int i = 1; i < N; i++) {
-			update = false; // dist 값이 변경되면 true로
 
-//			도시 1번 부터 N번까지
-			for (int j = 1; j <= N; j++) {
-				for (Edge edge : list[j]) {
-					if (dist[j] == Integer.MAX_VALUE)
-						break;
+//			엣지리스트 확인
+			for (int j = 0; j < M; j++) {
+				Edge edge = list[j];
 
 //					최단 거리 갱신이 있다면 dist배열에 바꿔주고 update true
-					if (dist[edge.vertex] > edge.value + dist[j]) {
-						dist[edge.vertex] = edge.value + dist[j];
-						update = true;
-					}
-
+				if (dist[edge.start] != Integer.MAX_VALUE && dist[edge.end] > edge.value + dist[edge.start]) {
+					dist[edge.end] = edge.value + dist[edge.start];
 				}
 
 			}
-			
 
 		}
-		
-//		음수사이클 존재 여부 확인을 위해 한 번더 돌리기
-		if(update) {
-			for(int i = 1; i <= N; i++) {
-				for(Edge edge : list[i]) {
-					
-					if (dist[i] == Integer.MAX_VALUE)
-						break;
 
-//					값 변화가 있다면 음수사이클 존재
-					if (dist[edge.vertex] > edge.value + dist[i]) {
-						dist[edge.vertex] = edge.value + dist[i];
-						return true;
-					}
-				}
+//		음수사이클 존재 여부 확인을 위해 한 번더 돌리기
+//		도시 1번 부터 N번까지
+		for (int i = 0; i < M; i++) {
+			Edge edge = list[i];
+
+//				최단 거리 갱신이 있다면 dist배열에 바꿔주고 update true
+			if (dist[edge.start] != Integer.MAX_VALUE && dist[edge.end] > edge.value + dist[edge.start]) {
+				return true;
 			}
-			
+
 		}
 
 		return false;
