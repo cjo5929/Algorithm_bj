cbb9c34d70613f308685f00e9cb192424b18773e
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-16 23:52:55 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 464 ms, Memory: 44132 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,44 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
 
+public class Main {
+	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static int[] dx = { -1, -1, -1 }; // 왼쪽 가운데 오른쪽
 	static int[] dy = { -1, 0, 1 };
-	static int N, M, D, sum_result, result;
+	static int N, M, D, sum_result, result, minD, minX, minY;
 	static int[][] arr, temp;
+	static boolean[][] visited;
+	static int[] archer_list;
 	static boolean game_over;
-	static int[] archer;
-	static List<int[]> list;
-
-	static class Monster implements Comparable<Monster> {
-		int x, y, d; // 행, 열, 거리
-
-		public Monster(int x, int y, int d) {
-			this.x = x;
-			this.y = y;
-			this.d = d;
-		}
-
-		@Override
-		public int compareTo(Monster o) {
-			if (this.d == o.d) {
-				return this.y - o.y;
-			} else {
-				return this.d - o.d;
-			}
-
-		}
-
-	}
 
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
@@ -46,109 +24,115 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		D = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		archer = new int[3];
-		list = new ArrayList<>();
+		arr = new int[N + 1][M];
+		temp = new int[N + 1][M];
+		archer_list = new int[3];
+		sum_result = 0;
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j] == 1) {
-					list.add(new int[] { i, j });
-				}
+				temp[i][j] = arr[i][j];
 			}
 		}
-
+		
+		
+		
 		comb(0, 0);
 
 		System.out.println(sum_result);
 
 	}
-
-	static void comb(int depth, int start) {
-		if (depth == 3) {
-			List<int[]> data = copy(list);
-			attack(data);
+//	arr 배열 다시 생성
+	static void init() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = temp[i][j];
+			}
+		}
+	}
+	
+	static void comb(int start, int depth) {
+		if(depth == 3) {
+			init();
+			attack(archer_list);
 			return;
 		}
-
-		for (int i = start; i < M; i++) {
-			archer[depth] = i;
-			comb(depth + 1, i + 1);
+		
+		for(int i = start; i < M; i++) {
+			archer_list[depth] = i;
+			comb(start + 1, depth + 1);
 		}
 	}
-
-//  몬스터 좌표 리스트 복사
-	static List<int[]> copy(List<int[]> list) {
-		List<int[]> temp = new ArrayList<>();
-		for (int i = 0; i < list.size(); i++) {
-			int[] cur = list.get(i);
-			temp.add(new int[] { cur[0], cur[1] });
-		}
-
-		return temp;
-	}
-
-//	공격
-	static void attack(List<int[]> list) {
-		result = 0; // 공격한 몬스터 수 초기화
-
-		while (true) {
-			if (list.isEmpty())
-				break;
-			List<int[]> targets = new ArrayList<>(); // 이번 공격에 공격 할 적의 좌표
-
-			for (int i = 0; i < 3; i++) {
-				PriorityQueue<Monster> q = new PriorityQueue<>();
-
-				for (int a = 0; a < list.size(); a++) {
-					int[] cur = list.get(a);
-					int d = Math.abs(cur[0] - N) + Math.abs(cur[1] - archer[i]);
-					if (d <= D) {
-						q.add(new Monster(cur[0], cur[1], d));
-					}
-				}
-
-				if (!q.isEmpty()) {
-					Monster target = q.poll();
-
-					boolean flag = false;
-					for (int j = 0; j < targets.size(); j++) {
-						int[] cur2 = targets.get(j);
-						if (target.x == cur2[0] && target.y == cur2[1]) { // 이미 누군가가 잡음
-							flag = true;
+	
+	static void attack(int[] archer_list) {
+		result = 0;
+		game_over = false;
+		for(int t = 0; t < N; t++) {
+			visited = new boolean[N + 1][M];
+			for(int k = 0; k < 3; k++) {
+				int archer_y = archer_list[k];
+				minD = Integer.MAX_VALUE;
+				minX = Integer.MAX_VALUE;
+				minY = Integer.MAX_VALUE;
+				
+				for (int i = 1; i <= N; i++) {
+					for (int j = 0; j < M; j++) {
+						if(arr[i][j] == 1) {
+							int dist = dist(i, j, N+1, archer_y);
+							if(minD >= dist) {
+								
+								if(minD > dist) {
+									minD = dist;
+									minX = i;
+									minY = j;
+								}else {
+									if(minY > j) {
+										minX = i;
+										minY = j;
+									}
+								}
+							}
 						}
 					}
-
-					if (!flag) {
-						targets.add(new int[] { target.x, target.y });
-					}
-
 				}
-
+//				공격할 수 있는 위치 visited에 true
+				if(minD <= D) {
+					visited[minX][minY] = true;
+				}
+				
 			}
-
-			for (int a = 0; a < targets.size(); a++) {
-				for (int b = list.size() - 1; b >= 0; b--) {
-					if (targets.get(a)[0] == list.get(b)[0] && targets.get(a)[1] == list.get(b)[1]) {
-						list.remove(b);
+			
+//			공격
+			for (int i = 1; i <= N; i++) {
+				for (int j = 0; j < M; j++) {
+					if(visited[i][j] == true) {
+						arr[i][j] = 0;
 						result++;
-						break;
 					}
 				}
 			}
-
-//			몬스터 이동
-			for (int i = list.size() - 1; i >= 0; i--) {
-				list.get(i)[0] += 1;
-				if (list.get(i)[0] == N)
-					list.remove(i);
+			
+//			성 바로 위줄 0으로 초기화 => 공격 끝나고 남은 마지막 줄 적들은 맵 밖으로 나감
+			for(int i = 0; i < M; i++) {
+				arr[N][i] = 0;
 			}
-
+			
+//			i - 1 값을 i로 => 적들 한 칸씩 내려오기 
+			for (int i = N; i > 0; i--) {
+				for (int j = 0; j < M; j++) {
+					 arr[i][j] = arr[i - 1][j];
+				}
+			}
+			
+			
 		}
-
-		sum_result = Math.max(result, sum_result);
+		sum_result = Math.max(sum_result, result);
+	}
+	
+	static int dist(int x1, int y1, int x2, int y2) {
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 	}
 
 }
