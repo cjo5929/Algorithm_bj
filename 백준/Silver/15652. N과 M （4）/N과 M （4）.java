5cad2305f783a2975d627c6fe70d174d8c6c249f
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-20 00:08:31 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 20016 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int n, r;
+	static boolean[] visited;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		visited = new boolean[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = i + 1;
+		}
+		
+
+		combi(new int[r], 0, 0);
+		System.out.println(sb);
+
+	}
+	static void combi(int[] copy, int depth, int start) {
+		if(depth == r) {
+			for(int i : copy) {
+				sb.append(i + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		
+		for(int i = start; i < n; i++) {
+			copy[depth] = arr[i];
+			combi(copy, depth + 1, i);
+		}
+	}
+
+}
