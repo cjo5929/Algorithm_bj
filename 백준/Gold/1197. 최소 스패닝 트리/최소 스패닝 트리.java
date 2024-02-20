48de6b1f35515e89c9ef8b93a942b1bffee08409
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-02-20 23:57:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 540 ms, Memory: 47568 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,7 +1,12 @@
+/**
+ * 1. 에지리스트로 구현하고 유니온 파인트 배열 초기화
+ * 2. 가중치 기준으로 오름차순 정렬
+ * 3. 가중치가 가장 낮은 에지부터 연결 시도 
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -16,6 +21,7 @@ public class Main {
 	static boolean[] visited;
 	static Queue<Edge> q = new PriorityQueue<Edge>();
 
+//	에지리스트
 	static class Edge implements Comparable<Edge> {
 		int start;
 		int end;
@@ -27,6 +33,7 @@ public class Main {
 			this.value = value;
 		}
 
+//		가중치 오름차순 
 		public int compareTo(Edge o) {
 
 			return this.value - o.value;
@@ -39,8 +46,9 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		parent = new int[V + 1];
-		
-		for (int i = 0 ; i < V + 1; i++) {
+
+//		1. 에지리스트로 구현하고 유니온 파인트 배열 초기화
+		for (int i = 0; i < V + 1; i++) {
 			parent[i] = i;
 		}
 
@@ -50,20 +58,22 @@ public class Main {
 			int end = Integer.parseInt(st.nextToken());
 			int value = Integer.parseInt(st.nextToken());
 
+			// 2. 가중치 기준으로 오름차순 정렬 => comparable 사용하고 우선순위 큐에 담으면 자동 정렬
 			q.add(new Edge(start, end, value));
 		}
 
-		
-		while(edge < V - 1) {
+//		MST를 만족하기 위해서는 에지 수가 최대 V - 1개
+		while (edge < V - 1) {
 			Edge cur = q.poll();
-			
-			if(find(cur.start) != find(cur.end)) {
+
+//		    3. 가중치가 가장 낮은 에지부터 연결 시도 => 대표노드가 서로 다를 경우! , 만약 같다면 사이클이 형성됨 
+			if (find(cur.start) != find(cur.end)) {
 				union(cur.start, cur.end);
 				result += cur.value;
 				edge++;
 			}
 		}
-		
+
 		sb.append(result);
 		System.out.println(sb);
 
