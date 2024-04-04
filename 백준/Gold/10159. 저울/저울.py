82e6e523305c2c517e2f301727c8afda9c4eae1f
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-04-04 09:33:32 +0900

    [Gold IV] Title: 저울, Time: 148 ms, Memory: 110852 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+m = int(sys.stdin.readline().rstrip())
+
+
+graph = [[100000] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = 1
+
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+
+for i in range(1, n + 1):
+    cnt = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != 100000 or graph[j][i] != 100000:
+            cnt += 1
+
+    print((n - 1) - cnt)
