189e9c8222969504a9188d4251ccc825e396670a
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-04-10 22:04:05 +0900

    [Gold IV] Title: 여왕벌, Time: 2784 ms, Memory: 117016 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,46 +1,38 @@
 import sys
 
 m, n = map(int, sys.stdin.readline().split())
-dx = [-1, 0]
-dy = [0, 1]
-maps = [[1] * m for _ in range(m)]
 
+maps = [[1] * m for _ in range(m)]
+worm = [0] * (m * 2 - 1)
 length = (m * 2)
 
 for _ in range(n):
     arr = list(map(int, sys.stdin.readline().split()))
-    dist = 0
-    idx = 0
-    x, y = m, 0
-    for i in range(length):
-        ax = x + dx[dist]
-        ay = y + dy[dist]
 
-        if ax < 0:
-            ax -= dx[dist]
-            ay -= dy[dist]
-            dist = 1
-            continue
+    for i in range(arr[0], arr[0] + arr[1]):
+        worm[i] += 1
+    for i in range(arr[0] + arr[1], (m * 2 - 1)):
+        worm[i] += 2
 
 
-        while (arr[idx] == 0):
-            idx += 1
+index = 0
+# 애벌레 성장
+for i in range(m - 1, -1, -1):
+    maps[i][0] += worm[index]
+    index += 1
 
+for i in range(1, m):
+    maps[0][i] += worm[index]
+    index += 1
 
-        arr[idx] -= 1
-        maps[ax][ay] += idx
-
-        x = ax
-        y = ay
-
+# 밑에 애벌레 크기 증가
 
 
 for i in range(m):
     for j in range(m):
 
-        if i  > 0 and j > 0:
+        if i > 0 and j > 0:
             maps[i][j] = maps[i - 1][j]
 
         print(maps[i][j], end=" ")
-
     print()
