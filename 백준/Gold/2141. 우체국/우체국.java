b7ac931f65a76241df32f4dd16c07132b44fe6cb
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-06-20 17:05:19 +0900

    [Gold IV] Title: 우체국, Time: 420 ms, Memory: 47732 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -7,49 +7,54 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static int N;
-	static long result, total;
-	static long[][] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static long result, total;
+    static long[][] arr;
 
-	public static void main(String[] args) throws IOException {
-		N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        arr = new long[n][2];
+        total = 0;
 
-		total = 0;
-		arr = new long[N][2];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Long.parseLong(st.nextToken());
+            arr[i][1] = Long.parseLong(st.nextToken());
+            total += arr[i][1];
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			total += arr[i][1];
-		}
+        }
 
-		Arrays.sort(arr, new Comparator<long[]>() {
+//       마을 위치를 기준으로 정렬
+        Arrays.sort(arr, new Comparator<long[]>(){
 
-			@Override
-			public int compare(long[] o1, long[] o2) {
-				if (o1[0] == o2[0]) return (int)(o1[1] - o1[1]);
-	
-				return (int)(o1[0] - o2[0]);
-			}
-		});
+            @Override
+            public int compare(long[] o1, long[] o2) {
+                if (o1[0] == o2[0]){
+                    return (int)(o1[1] - o2[1]);
+                }
+                return (int)(o1[0] - o2[0]);
+            }
+        });
 
-		long mid = (total + 1) / 2;
-		for (long[] point : arr) {
-			result += point[1];
 
-			if (result >= mid) {
-				sb.append(point[0]);
-				break;
-			}
+        long mid = (total + 1) / 2;
 
-		}
-		
-		System.out.println(sb);
+//      total 인구수의 중간 값 확인
+        for(long[] point : arr){
+            result += point[1];
 
-	}
+//         중간값을 넘는 첫 번째 마을의 위치
+            if(result >= mid){
+                sb.append(point[0]);
+                break;
+            }
+        }
 
+
+        System.out.println(sb);
+
+    }
 }
