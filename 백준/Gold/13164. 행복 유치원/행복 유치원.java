f3eba28d5d8c00df27560a52943299992fca7e70
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-08-22 17:37:22 +0900

    [Gold V] Title: 행복 유치원, Time: 608 ms, Memory: 70760 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,32 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+import org.w3c.dom.Node;
+
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
     static int n, k, result;
+    static ArrayList<Integer> list;
     static int[] arr;
-    static ArrayList<Integer> list = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException {
+        list = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        arr = new int[n];
+        for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < n; i++) {
+        for(int i = 1; i < n; i++){
             list.add(arr[i] - arr[i - 1]);
         }
 
@@ -37,7 +42,6 @@ public class Main {
         }
 
         System.out.println(result);
-
 
     }
 }
