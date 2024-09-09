c75e240d3c43b40ae4ec5b4cd84130fe1e0642e7
minseo kang <90138160+cjo5929@users.noreply.github.com>
2024-09-09 23:59:05 +0900

    [level 3] Title: 등굣길, Time: 0.97 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    
+    static int[][] dp;
+    
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        dp = new int[n][m];
+        
+        for(int[] a : puddles) {
+            dp[a[1] - 1][a[0] - 1] = -1;
+        }
+        
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                if(i != 0) {
+                  dp[i][j] += dp[i - 1][j] % 1000000007;
+
+                }
+                
+                if(j != 0){
+                    dp[i][j] += dp[i][j - 1] % 1000000007;
+                }
+                
+                
+            }
+        }
+        
+        
+        
+        return dp[n - 1][m - 1] % 1000000007;
+    }
+}
